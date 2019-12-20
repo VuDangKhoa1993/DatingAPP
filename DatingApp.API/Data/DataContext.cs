df7907294eb0f4566ea6fb88ba4fe3a7516a14a3
dangkhoa <dangkhoavu1993@gmail.com>
2019-12-20 23:02:20 +0800

    end of section authentication

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -15,14 +15,5 @@ namespace DatingApp.API.Data
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
         
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer("@");
-        //}
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    //modelBuilder.Entity<Value>().HasKey("Id").
-        //}
     }
 }
